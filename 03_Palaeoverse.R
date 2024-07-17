6e4cbbc9e987f27d3f703b78db1f20b6dd9c9847
Bethany Allen <39991015+bethany-j-allen@users.noreply.github.com>
2024-07-17 16:35:11 +0200

    Link to vignettes

--- 03_Palaeoverse.R
+++ 03_Palaeoverse.R
@@ -1,1 +1,18 @@
 #Palaeoverse vignette
+
+#[Palaeoverse](palaeoverse.palaeoverse.org) is an R package containing functions
+#specifically for cleaning and exploring fossil occurrence data. It is designed
+#to be used prior to any specific analyses you wish to undertake.
+
+#Two vignettes are provided with the palaeoverse R package. These are:
+#- [Latitudinal trends in Phanerozoic reefs](github.com/palaeoverse/palaeoverse/blob/main/vignettes/phanerozoic-reefs.Rmd)
+#- [Alpha diversity and geographic range size of Carboniferous and Permian tetrapods](palaeoverse/palaeoverse/blob/main/vignettes/tetrapod-biodiversity.Rmd)
+
+#Walking through these vignettes should give you an idea of the functionality
+#available in the package and how this might be applied to a dataset.
+
+#You can also see the *Methods in Ecology and Evolution* paper introducing the
+# package [here](doi.org/10.1111/2041-210X.14099)
+
+#If you have any questions, please feel free to email Bethany, or any of the
+#package development team!
